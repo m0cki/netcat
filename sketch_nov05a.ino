d04e6543bca11557dbd89e1b5bab79267157ac31
Paul <148272869+m0cki@users.noreply.github.com>
2024-11-08 11:02:19 +0100

    Update sketch_nov05a.ino

--- sketch_nov05a.ino
+++ sketch_nov05a.ino
@@ -1,21 +1,20 @@
-
-const int clockPin = 2;
-const int outPins[3] = {3,4,5};
-const int inPins[3] = {6,7,8};
-volatile bool isRisingEdge = true;
+ const int clockPin = 2;
+ const int outPins[3] = {3,4,5};
+ const int inPins[3] = {6,7,8};
+ volatile bool isRisingEdge = true;
+  
 
 void setup() {
-  pinMode(clockpin, INPUT);
+  pinMode(clockPin, INPUT);
   for(int i=0;i<3;i++){
     pinMode(outPins[i], OUTPUT);
     pinMode(inPins[i], INPUT);  
   }
-  attachInterrupt(digitalPinToInterrupt(clockPin), handleClock, CHANGE)
+  attachInterrupt(digitalPinToInterrupt(clockPin), handleClock, CHANGE);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+ 
 }
 
 void handleClock(){
@@ -30,12 +29,15 @@ void handleClock(){
 
 void sendData(){
   //hier sollen die 3 Output Pins gesetzt werden
+  digitalWrite(outPins[0], HIGH);
+  digitalWrite(outPins[1], LOW);
+  digitalWrite(outPins[2], HIGH);
 }
 
 void receiveData(){
   //hier sollen die 3 Input Pins gesetzt werden
   int receivedBits[3];
-  //receivedBits[0] =
-  //receivedBits[1] =
-  //receivedBits[2] =
+  //receivedBits[0] = digitalWrite(inPins[0]);
+  //receivedBits[1] = digitalWrite(inPins[1]);
+  //receivedBits[2] = digitalWrite(inPins[2]);
 }
